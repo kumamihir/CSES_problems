4cd30f29743305eb303b51cb0ec5dfd235826d35
MIHIR KUMAR <mihirkumar1208@gmail.com>
2026-07-01 02:29:51 +0530

    qn 4

---
+++ binarysearch/4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    multiset<int> tickets;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        tickets.insert(x);
+    }
+
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+
+        auto it = tickets.upper_bound(x);
+
+        if (it == tickets.begin()) {
+            cout << -1 << '\n';
+        } else {
+            --it;
+            cout << *it << '\n';
+            tickets.erase(it);
+        }
+    }
+
+    return 0;
+}
