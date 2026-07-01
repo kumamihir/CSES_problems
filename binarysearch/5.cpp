2b5a4f3c9fac9b3df59e2a3d6fd49478fcb55d28
MIHIR KUMAR <mihirkumar1208@gmail.com>
2026-07-02 00:17:37 +0530

    qn 5

---
+++ binarysearch/5.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+#define ld long double
+
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define sz(x) (int)(x).size()
+
+#define endl '\n'
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+   int n;
+cin >> n;
+
+vector<pair<int,int>> arr(n);
+
+for(int i = 0; i < n; i++){
+    cin >> arr[i].first >> arr[i].second;
+}
+priority_queue<int, vector<int>, greater<int>> pq;
+sort(all(arr));
+int mxc=0;
+for(auto [start,end] : arr){
+    while(!pq.empty() && pq.top()<start){
+        pq.pop();
+    }
+    pq.push(end);
+    mxc=max(mxc,(int)pq.size());
+}
+
+cout<<mxc;
+
+
+
+    return 0;
+}
